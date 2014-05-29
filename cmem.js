865c8662a45c1c46a038341c3454ce84236e646e
neytema <neytema@gmail.com>
2014-05-29 21:56:40 +0300

    Main file

---
+++ cmem.js
@@ -0,0 +1,132 @@
+'use strict';
+
+var patterns = {};
+var slice = Array.prototype.slice;
+
+module.exports = create();
+
+function create() {
+  Object.defineProperties(cmem, {
+    create: { value: create },
+    clear: { value: clear },
+    unit: { value: unit },
+    iterator: { value: iterator },
+    memos: { value: [] }
+  });
+
+  return cmem;
+
+  function cmem(fn) {
+    var result = make(fn);
+    cmem.memos.push(result);
+    return result;
+  }
+}
+
+function make(fn) {
+  var $calls = [], fn_;
+
+  Object.defineProperties(ctx, {
+    $clear: { value: $clear },
+    $fn: { value: $fn },
+    $unit: { value: $unit },
+
+    $map: { value: $map },
+    $filter: { value: $filter },
+    $select: { value: $select },
+
+    $calls: { value: $calls },
+    $last: { value: null, writable: true },
+    $args: { value: null, writable: true },
+    $this: { value: null, writable: true },
+    $count: { value: 0, writable: true },
+  });
+
+  return $clear();
+
+  function ctx() {
+    ctx.$count = $calls.push(ctx.$last = {
+      $args: ctx.$args = arguments,
+      $this: ctx.$this = this
+    });
+
+    return (fn_ || fn || _silent).apply(this, arguments);
+  }
+
+  function $clear() {
+    $calls.length = 0;
+    ctx.$last = null;
+    ctx.$args = null;
+    ctx.$this = null;
+    ctx.$count = 0;
+
+    return ctx;
+  }
+
+  function $fn(fn) {
+    return (fn_ = fn, ctx);
+  }
+
+  function $unit(unit_) {
+    return $fn(unit(unit_));
+  }
+
+  function $map() {
+    return _iterate($calls, 'map', arguments, ctx, this);
+  }
+
+  function $filter() {
+    return _iterate($calls, 'filter', arguments, ctx, this);
+  }
+
+  function $select(filter, map) {
+    var args = slice.call(arguments, 1), result;
+    args[0] = filter;
+
+    result = $filter.apply(this, args);
+
+    if (map) {
+      args[0] = map;
+      result = _iterate(result, 'map', args, ctx, this);
+    }
+
+    return result;
+  }
+}
+
+function iterator(pattern) {
+  var result = patterns[pattern];
+
+  if ( ! result) {
+    result = Function('$item,$i,$list,$','$=arguments.callee.$;with($item)return '+ pattern);
+    Object.defineProperty(result, '$', { value: [], configurable: true, writable: true });
+    patterns[pattern] = result;
+  }
+
+  result.$ = slice.call(arguments, 1);
+
+  return result;
+}
+
+function unit(value) {
+  return function bond() {
+    return value;
+  };
+}
+
+function clear() {
+  var a = this.memos, i = a.length;
+  while (i--) a[i].$clear();
+}
+
+function _iterate(list, action, args, ctx, self) {
+  var pattern = args[0];
+
+  if (typeof pattern == 'function') {
+    return list[action].apply(list, args);
+  } else {
+    return list[action](iterator.apply(null, args), ctx !== self ? self : undefined);
+  }
+}
+
+function _silent() {}
